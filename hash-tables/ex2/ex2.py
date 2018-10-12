187321b466d46fc6e3b905c3e71ea70b17e787f5
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-10-12 14:02:57 -0700

    convert ex2 from list solution to hash/dictionary solution

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,14 +1,38 @@
 def reconstruct_trip(tickets):
   itinerary = []
 
-  while len(itinerary) != len(tickets) - 1:
-    for tripTuple in tickets: 
-      arrival = tripTuple[0]
-      destination = tripTuple[1]
-      if arrival == None and destination not in itinerary: itinerary.insert(0,destination)
-      elif destination == None and arrival not in itinerary: itinerary.append(arrival)
-      elif destination and arrival in itinerary and destination not in itinerary : itinerary.insert(itinerary.index(arrival)+1, destination)
-      elif arrival and destination in itinerary  and arrival not in itinerary : itinerary.insert(itinerary.index(destination), arrival)
+#solution without hash table
+  # while len(itinerary) != len(tickets) - 1:
+  #   for tripTuple in tickets: 
+  #     arrival = tripTuple[0]
+  #     destination = tripTuple[1]
+  #     if arrival == None and destination not in itinerary: itinerary.insert(0,destination)
+  #     elif destination == None and arrival not in itinerary: itinerary.append(arrival)
+  #     elif destination and arrival in itinerary and destination not in itinerary : itinerary.insert(itinerary.index(arrival)+1, destination)
+  #     elif arrival and destination in itinerary  and arrival not in itinerary : itinerary.insert(itinerary.index(destination), arrival)
+
+  
+  #create hash table
+  ht = {}
+
+  for tripTuple in tickets:
+    arrival = tripTuple[0]
+    destination = tripTuple[1]
+    if arrival == None: itinerary.insert(0, destination)
+    # elif destination == None: itinerary.append(arrival)
+    else:
+      ht[arrival] = destination
+  
+  #create itinerary
+  arrival = 1 #default start
+  destination = itinerary[0]
+  while arrival != None:
+    arrival = ht[destination]
+    
+    if arrival: 
+      itinerary.append(arrival)
+      destination = ht[arrival]
+      if destination: itinerary.append(destination)
 
 
   return itinerary
